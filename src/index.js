56b40a6a6c99f5126962adfc58862530d23bcb6c
Alexander invntrm Mextner <a-x-@users.noreply.github.com>
2017-07-09 08:05:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,58 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import './index.css';
 
-export default class PrintProvider extends React.PureComponent {
+class PrintProvider extends React.PureComponent {
   getChildContext () {
     return {
-      regPrintable: this.regPrintable.bind(this),
+      printProvider: {
+        isPrint: window.matchMedia('print').matches,
+      },
     };
-  }
-
-  constructor (props) {
-    super(props);
-    this.state = {
-      isInPrintPreview: false,
-      printableNodes: [],
-      printableRegistry: {},
-    };
-    window.matchMedia('print').onchange = () => {
-      this.setState({ isInPrintPreview: window.matchMedia('print').matches });
-    };
-  }
-
-  regPrintable (key, node) {
-    if (this.state.printableRegistry[key]) return;
-    this.setState({
-      printableNodes: this.state.printableNodes.concat(node),
-      printableRegistry: Object.assign({}, printableRegistry, { [key]: true }),
-    });
   }
 
   render () {
-    if (this.state.isInPrintPreview) {
-      return React.Children.map(this.state.printableNodes, (child, key) => {
-        return React.cloneElement(child, { key });
-      });
-    }
-
-    return this.props.children;
+    return <div className="print-provider__wrap">{ this.props.children }</div>;
   }
 }
 
 PrintProvider.childContextTypes = {
   printProvider: PropTypes.shape({
-    regPrintable: PropTypes.func.isRequired,
+    isPrint: PropTypes.bool.isRequired,
   }).isRequired,
 };
 
-export class Print extends React.PureComponent {
-  constructor (props, context) {
-    super(props);
-    context.printProvider.regPrintable(props.name, this)
-  }
-  render () {
-    return this.props.children;
-  }
-}
+const Print = () => {
+  return <div className="print-provider__print">{ this.props.children }</div>;
+};
 
-Print.contextTypes = PrintProvider.childContextTypes;
+const NoPrint = () => {
+  return <div className="print-provider__no-print">{ this.props.children }</div>;
+};
+
+export default PrintProvider;
+export { Print, NoPrint };
