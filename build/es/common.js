b0a1a3e54cf918a3a9d7eb9f298860993f777b19
alex <alex@rocketguys.com>
2018-06-18 20:53:50 +0300

    build

---
+++ build/es/common.js
@@ -0,0 +1,3 @@
+export var spliced=function spliced(array,i){var array_=[].concat(babelHelpers.toConsumableArray(array));array_.splice(i,1);return array_};export var debug=function debug(){var _console;if(!window.localStorage['react-easy-print-debug'])return;(_console=console).log.apply(_console,arguments);// eslint-disable-line
+};
+//# sourceMappingURL=common.js.map
---
+++ build/es/index.js
@@ -0,0 +1,2 @@
+export{default as PrintProvider}from'./PrintProvider';export{default}from'./PrintProvider';export{default as NoPrint}from'./NoPrint';export{default as Print}from'./Print';
+//# sourceMappingURL=index.js.map
